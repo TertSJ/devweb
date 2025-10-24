e8ffd784e47a03536d4389649453f72a5b9ecb83
TertulianoSJ <tertuliano.s.j@gmail.com>
2025-10-24 10:21:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function verificacao(){
+    let nomeInput = document.getElementById("name");
+    let idadeInput = document.getElementById("idade");
+    let mensagemResultado = document.getElementById("resultado");
+
+    let nome = nomeInput.ariaValueMax.trim();
+    let idade = parseInt(idadeInput.value);
+
+    let vereador = 18;
+    let prefeitoDeputadoJuizPaz = 21;
+    let governador = 30;
+    let senadorPresidente = 35;
+    
+}
