4b201e4e3804dff21aee4817c89be1a9ec20f2d6
TertulianoSJ <tertuliano.s.j@gmail.com>
2025-10-31 10:32:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 function verificacao(){
-    let nomeInput = document.getElementById("name");
+    let nomeInput = document.getElementById("nome");
     let idadeInput = document.getElementById("idade");
     let mensagemResultado = document.getElementById("resultado");
 
-    let nome = nomeInput.ariaValueMax.trim();
+    let nome = nomeInput.value.trim();
     let idade = parseInt(idadeInput.value);
 
     let vereador = 18;
     let prefeitoDeputadoJuizPaz = 21;
     let governador = 30;
     let senadorPresidente = 35;
+    let mensagem = "";
+    let cargosElegiveis = [];
+
+    if (!nome || isNaN(idade)){
+        mensagem = "Nome e Idade são obrigatórios";
+        mensagemResultado.innerHTML=mensagem;
+        return;
+    }
+    if (idade<vereador){
+        mensagem = "Você não atende a Idade mínima de para se tornar um vereador.";
+        mensagemResultado.innerHTML=mensagem;
+      
+    }else{
+        cargosElegiveis.push("Vereador");
+        if (idade>=prefeitoDeputadoJuizPaz){
+            cargosElegiveis.push("Prefeito"); 
+            cargosElegiveis.push("Deputado"); 
+            cargosElegiveis.push("Juiz de Paz");
+            if(idade>=governador){
+                cargosElegiveis.push("Governador");
+                if (idade>=senadorPresidente){
+                    cargosElegiveis.push("Senador"); 
+                    cargosElegiveis.push("Presidente");
+                }
+            }
+
+        }
+        mensagem = `${nome} pode ser eleito: \n <br/> ${cargosElegiveis.join("<br><hr>")} `;
+       
+    }
     
+    mensagemResultado.innerHTML=mensagem;
+}   
+function mudarCorDeFundo(cor){
+    document.body.style.backgroundColor=cor;
 }
+let corEscolhida = prompt("Digite o nome de uma cor em inglês(ex: red, blue, green):")
+mudarCorDeFundo(corEscolhida);
